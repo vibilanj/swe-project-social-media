e9adf2388049d18aa7ddc21320c5f71ead8b5baf
Vibilan Jayanth <vibilan@firstindo.com>
2023-03-14 14:06:13 +0800

    navigation sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Card from "@/components/Card";
+import NavigationCard from "@/components/NavigationCard";
 
 export default function Home() {
   return (
     <div className='flex mt-4 max-w-4xl mx-auto gap-6'>
       <div className="w-1/3">
-        <Card>
-          links here
-        </Card>
+        <NavigationCard />
       </div>
       <div className="grow">
         <Card>form here</Card>
