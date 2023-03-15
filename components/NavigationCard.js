05b68ee8bb7ce19bec70f79d481044a949a30a15
Vibilan Jayanth <vibilan@firstindo.com>
2023-03-15 18:20:36 +0800

    saved posts

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -25,7 +25,7 @@ export default function NavigationCard() {
           </svg>
           Friends
         </Link>
-        <Link href="" className={nonActiveElementClasses}>
+        <Link href="/saved" className={pathname === '/saved' ? activeElementClasses : nonActiveElementClasses}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
           </svg>
