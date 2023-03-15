7c87f56801f57926b2fb5c67dabb2cd6c985eb61
Vibilan Jayanth <vibilan@firstindo.com>
2023-03-15 13:27:32 +0800

    single post

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
+import Avatar from "@/components/Avatar";
 import Card from "@/components/Card";
 import NavigationCard from "@/components/NavigationCard";
+import PostCard from "@/components/PostCard";
 import PostFormCard from "@/components/PostFormCard";
 
 export default function Home() {
   return (
     <div className='flex mt-4 max-w-4xl mx-auto gap-6'>
-      <div className="w-1/3">
+      <div className="w-1/4">
         <NavigationCard />
       </div>
-      <div className="grow">
+      <div className="w-3/4">
         <PostFormCard />
-        <Card>first post test</Card>
+        <PostCard />
       </div>
     </div>
   )
