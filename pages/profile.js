56cc55cd92c53f996e4389540acc42bed7419781
Vibilan Jayanth <vibilan@firstindo.com>
2023-03-23 11:43:22 +0800

    update name, city

--- pages/profile.js
+++ pages/profile.js
@@ -11,10 +11,14 @@ import { useEffect, useState } from "react";
 
 export default function ProfilePage() {
   const [profile, setProfile] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+  const [name, setName] = useState('');
+  const [place, setPlace] = useState('');
+
   const router = useRouter();
   const session = useSession();
   const userId = router.query.id;
-  const { asPath:pathname } = router;
+  const { asPath: pathname } = router;
   const supabase = useSupabaseClient();
 
   useEffect(() => {
@@ -37,7 +41,21 @@ export default function ProfilePage() {
         }
       });
   }
-  
+
+  function saveProfile() {
+    supabase.from('profiles')
+      .update({
+        name,
+        place,
+      })
+      .eq('id', session.user.id)
+      .then(result => {
+        if (!result.error) {
+          setProfile(prev => ({ ...prev, name, place }));
+        }
+        setEditMode(false);
+      });
+  }
 
   const isPosts = pathname.includes('posts') || pathname === '/profile';
   const isAbout = pathname.includes('about');
@@ -49,7 +67,7 @@ export default function ProfilePage() {
 
   const isMyUser = userId === session?.user?.id;
 
-  return(
+  return (
     <Layout>
       <Card noPadding={true}>
         <div className="relative overflow-hidden rounded-md">
@@ -60,12 +78,66 @@ export default function ProfilePage() {
             )}
           </div>
           <div className="p-4 pt-0 md:pt-4 pb-0">
-            <div className="ml-24 md:ml-40">
-              <h1 className="text-3xl font-bold">
-                {profile?.name}
-              </h1>
-              <div className="text-gray-500 leading-4">
-                {profile?.place} 
+            <div className="ml-24 md:ml-40 flex justify-between">
+              <div>
+                {editMode && (
+                  <div>
+                    <input
+                      type="text"
+                      className="border py-2 px-3 rounded-md "
+                      placeholder={'Your name'}
+                      onChange={ev => setName(ev.target.value)}
+                      value={name} />
+                  </div>
+                )}
+                {!editMode && (
+                  <h1 className="text-3xl font-bold">
+                    {profile?.name}
+                  </h1>
+                )}
+                {!editMode && (
+                  <div className="text-gray-500 leading-4">
+                    {profile?.place}
+                  </div>
+                )}
+                {editMode && (
+                  <div>
+                    <input
+                      type="text"
+                      className="border py-2 px-3 rounded-md mt-1"
+                      placeholder={'Your location'}
+                      onChange={ev => setPlace(ev.target.value)}
+                      value={place} />
+                  </div>
+                )}
+              </div>
+              <div className="grow">
+                <div className="text-right">
+                  {isMyUser && !editMode && (
+                    <button
+                      onClick={() => {
+                        setEditMode(true);
+                        setName(profile.name);
+                        setPlace(profile.place);
+                      }}
+                      className="inline-flex mx-1 gap-1 bg-white rounded-md shadow-sm shadow-gray-500 py-1 px-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                      </svg>
+                      Edit profile
+                    </button>
+                  )}
+                  {isMyUser && editMode && (
+                    <button onClick={saveProfile} className="inline-flex gap-1 bg-white rounded-md shadow-sm shadow-gray-500 py-1 px-2">
+                      Save profile
+                    </button>
+                  )}
+                  {isMyUser && editMode && (
+                    <button onClick={() => setEditMode(false)} className="inline-flex gap-1 bg-white rounded-md shadow-sm shadow-gray-500 py-1 px-2">
+                      Cancel
+                    </button>
+                  )}
+                </div>
               </div>
             </div>
             <div className="mt-4 md:mt-10 flex gap-1">
@@ -114,30 +186,30 @@ export default function ProfilePage() {
       {isFriends && (
         <div>
           <Card>
-           <h2 className="text-3xl mb-2">Friends</h2>
-           <div className="">
-            <div className="border-b border-b-gray-100 p-4 -mx-4">
-              <FriendInfo />
+            <h2 className="text-3xl mb-2">Friends</h2>
+            <div className="">
+              <div className="border-b border-b-gray-100 p-4 -mx-4">
+                <FriendInfo />
+              </div>
+              <div className="border-b border-b-gray-100 p-4 -mx-4">
+                <FriendInfo />
+              </div>
+              <div className="border-b border-b-gray-100 p-4 -mx-4">
+                <FriendInfo />
+              </div>
+              <div className="border-b border-b-gray-100 p-4 -mx-4">
+                <FriendInfo />
+              </div>
+              <div className="border-b border-b-gray-100 p-4 -mx-4">
+                <FriendInfo />
+              </div>
+              <div className="border-b border-b-gray-100 p-4 -mx-4">
+                <FriendInfo />
+              </div>
+              <div className="border-b border-b-gray-100 p-4 -mx-4">
+                <FriendInfo />
+              </div>
             </div>
-            <div className="border-b border-b-gray-100 p-4 -mx-4">
-              <FriendInfo />
-            </div>
-            <div className="border-b border-b-gray-100 p-4 -mx-4">
-              <FriendInfo />
-            </div>
-            <div className="border-b border-b-gray-100 p-4 -mx-4">
-              <FriendInfo />
-            </div>
-            <div className="border-b border-b-gray-100 p-4 -mx-4">
-              <FriendInfo />
-            </div>
-            <div className="border-b border-b-gray-100 p-4 -mx-4">
-              <FriendInfo />
-            </div>
-            <div className="border-b border-b-gray-100 p-4 -mx-4">
-              <FriendInfo />
-            </div>
-           </div>
           </Card>
         </div>
       )}
