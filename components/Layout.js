35ee7012922772b22279d1160e33824473b0b14c
Marcellinus Jerricho <marcellinus.jerricho@u.yale-nus.edu.sg>
2023-03-29 15:49:35 +0800

    moved navigaiton

--- components/Layout.js
+++ components/Layout.js
@@ -11,7 +11,7 @@ export default function Layout({children, hideNavigation}) {
   return (
     <div className='md:flex mt-4 max-w-4xl mx-auto gap-6 mb-24 md:mb-0'>
       {!hideNavigation && (  
-        <div className="fixed md:static w-full bottom-0 md:w-1/4 -mb-5">
+        <div className="fixed md:static w-full bottom-0 md:w-1/4 -mb-5 z-10">
           <NavigationCard />
         </div>
       )}
